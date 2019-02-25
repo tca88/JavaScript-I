c4117907da0cf0c6021ecd2c910c4ea65e689e99
tca88 <trishnaamin@gmail.com>
2019-02-25 18:21:03 -0500

    Complete main challengeswithin function-conversion.js and complete stretch challenge.

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,48 @@
 // };
 // myFunction();
 
+//ANSWER
+let myFunction = () => 
+console.log("Function was invoked!");
+myFunction();
+
+/////////////////////////////////////////////////////
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+//ANSWER
+let anotherFunction = (param) => param;
+anotherFunction("Example");
+console.log(anotherFunction("Example"));
+
+/////////////////////////////////////////////////////
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+//ANSWER
+let add = (param1, param2) => param1 + param2;
+add(1,2);
+console.log(add(1,2));
+
+/////////////////////////////////////////////////////
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+//ANSWER
+let subtract = (param1, param2) => param1 - param2;
+subtract(1,2);
+console.log(subtract(1,2));
+
+/////////////////////////////////////////////////////
 
 // Stretch
 
@@ -28,3 +55,8 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+//ANSWER
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map(num => num*3);
+console.log(triple);
