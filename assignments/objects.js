5d5741eff06421b2111504eb5d60beeebbd4c8b7
tca88 <trishnaamin@gmail.com>
2019-02-25 17:57:02 -0500

    Complete main challenges within objects.js.

--- assignments/objects.js
+++ assignments/objects.js
@@ -55,20 +55,37 @@ const antonietta = {"id": 5,
 
 // Mitzi's name
 
+console.log(mitzi.name);
+
 // Kennan's ID
+
+console.log(kennan.id);
 
 // Keven's email
 
+console.log(keven.email);
+
 // Gannie's name
+
+console.log(gannie.name);
 
 // Antonietta's Gender
 
+console.log(antonietta.gender);
+
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+kennan.speak = function() {
+  return "Hello, my name is Kennan!";
+}
+
+console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+antonietta.multiplyNums = function(num1, num2) {
+  return num1 * num2;
+}
+console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
